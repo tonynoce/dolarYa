b754b4f33c21be04436fac10adfed5c16d7650c1
tonynoce <luke.1917@gmail.com>
2023-01-08 18:20:24 -0300

    added vibration, offline features

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,31 +25,33 @@
 
 	function offlineModeOn() {
 		offline = true;
+
 		$USDprice = Number(localStorage.getItem('ARSprice'));
 		$ARSprice = Number(localStorage.getItem('USDprice'));
 		lastUpdated = localStorage.getItem('Date');
 		theDate = new Date(Number(lastUpdated));
 	}
 
-	/*
-	function addToFavorites() {
-		if (browser) {
-			var title = document.title;
-			var url = document.location;
-			try {
-				window.external.AddFavorite(url, title);
-				AddFavorite(url, title);
-			} catch (e) {}
-			try {
-				window.sidebar.addPanel(title, url, '');
-			} catch (e) {
-				alert(
-					'Su navegador no nos deja agregar a favoritos.\n\nPlease use Ctrl+D to bookmark this page.'
-					);
-				}
-			}
-		}
-	*/
+	if (browser) {
+		addEventListener('deviceorientation', (event) => {
+			/* console.log('I moved !'); */
+		});
+		addEventListener('online', () => {
+			/* console.log('Onlineeee'); */
+			offline = false;
+		});
+		addEventListener('offline', () => {
+			/* console.log('offlineeeee'); */
+			offlineModeOn();
+		});
+
+		/* 		if (navigator.onLine) {
+			console.log('online');
+		} else {
+			console.log('offline');
+			offline = true;
+		} */
+	}
 </script>
 
 <svelte:head>
@@ -69,34 +71,43 @@
 	{:then}
 		<Swap {offline} {theDate} />
 	{:catch error}
-		{#if offline == true}
-			<div class="text-center text-white text-center font-bold">
-				<h1 class="text-2xl">Modo Offline activado:</h1>
-				<!-- 
+		{#key offline}
+			{#if offline == true}
+				<div class="text-center text-white text-center font-bold">
+					<h1 class="text-2xl">Modo Offline activado:</h1>
+					<!-- 
 					<p>Último precio de la cotización del día:</p>
 					<div>{theDate.toUTCString()}</div>
 				-->
-			</div>
-			<Swap {offline} {theDate} />
-		{:else}
-			<div class="loader text-center">
-				<div class="error">
-					<h1>Se produjo el siguiente error:</h1>
-					<p style="font-size:32pt;color: rgb(240, 46, 170)">{error}</p>
 				</div>
-				<div>
-					<Button
-						shadow="green"
-						gradient
-						color="alternative"
-						size="xl"
-						on:click={() => {
-							offlineModeOn();
-						}}>Modo Offline</Button
-					>
+				<Swap {offline} {theDate} />
+			{:else}
+				<div class="loader text-center">
+					<h1 class="text-2xl">No se detectó una conexión</h1>
+					<p>Activar el modo offline ?</p>
+					<br />
+					<div class="margin-top: 24pt;">
+						<Button
+							shadow="green"
+							gradient
+							color="alternative"
+							size="xl"
+							on:click={() => {
+								offlineModeOn();
+							}}>Modo Offline</Button
+						>
+					</div>
+					<!-- 
+
+						<div class="error">
+							<h1>Se produjo el siguiente error:</h1>
+							<p style="font-size:32pt;color: rgb(240, 46, 170)">{error}</p>
+						</div>
+					-->
+					<!-- Offline Div -->
 				</div>
-			</div>
-		{/if}
+			{/if}
+		{/key}
 	{/await}
 	<!-- 
 		<div class="favoritos">
